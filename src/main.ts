0cf5698bef51bbb6c5eceecf3e2e62121f49ea76
Will Schenk <wschenk@gmail.com>
2024-08-08 19:25:57 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,4 +23,28 @@ window.addEventListener("load", async () => {
       clerk.openSignIn();
     });
   }
+
+  // Example of a api query to a backend
+  const mainButton = document.getElementById("main-button");
+
+  mainButton?.addEventListener("click", async () => {
+    const token = await clerk.session?.getToken();
+    const results = document.getElementById("results");
+    if (!token || !results) {
+      if (results) {
+        results.textContent = "Error: Unable to get authentication token";
+      }
+      return;
+    }
+
+    const response = await fetch("http://localhost:9292/private", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    results.textContent = JSON.stringify(data);
+  });
 });
